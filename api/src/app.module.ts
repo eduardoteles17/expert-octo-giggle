c70726df3cc85b098f3cc0128c274b3f254b3007
Eduardo Teles <edu.teles@pm.me>
2023-10-12 14:11:28 -0300

    add @nestjs/config to api project
    Co-authored-by: juliacorre <juliacorrea2004@outlook.com>

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
-import { DeviceModule } from "./modules/device/device.module";
+import { DeviceModule } from './modules/device/device.module';
+import { ConfigModule } from '@nestjs/config';
+import { appConfigurations } from './config/configurations';
 
 @Module({
-  imports: [DeviceModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: appConfigurations,
+    }),
+    DeviceModule,
+  ],
 })
 export class AppModule {}
