8dad1c195c5c3b23671981ef8812a8943ab31144
eduardoteles17 <edu.teles@pm.me>
2024-05-25 18:23:16 -0300

    use diferent endpoint in alexa webhook
    Co-authored-by: juliacorre <juliacorrea2004@outlook.com>

--- api/src/modules/webhook/webhook.controller.ts
+++ api/src/modules/webhook/webhook.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { DeviceService } from '../device/device.service';
+import { DeviceStatus } from '../../enums/device-status.enum';
 
 @Controller('webhook')
 export class WebhookController {
   constructor(readonly deviceService: DeviceService) {}
 
-  @Post('/alexa')
-  async alexaWebhook(@Req() req: any) {
-    const data = req.body;
-    return this.deviceService.setStatusById(data.deviceId, data.status);
+  @Post('/alexa/active')
+  async alexaWebhook() {
+    return this.deviceService.setStatusById('smart-lamp', DeviceStatus.active);
+  }
+
+  @Post('/alexa/inactive')
+  async alexaInactiveWebhook() {
+    return this.deviceService.setStatusById(
+      'smart-lamp',
+      DeviceStatus.inactive,
+    );
   }
 }
