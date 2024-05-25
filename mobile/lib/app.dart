44f07c4b45cd5ebd33665156a7c55c809515d4c7
eduardoteles17 <edu.teles@pm.me>
2024-05-25 15:32:50 -0300

    add splash screen
    Co-authored-by: juliacorre <juliacorrea2004@outlook.com>

--- mobile/lib/app.dart
+++ mobile/lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/screens/home_screen.dart';
+import 'package:mobile/screens/splash_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+        home: SplashScreen(),
     );
   }
 }
---
+++ mobile/lib/screens/splash_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  _initApp() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (mounted) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initApp();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset(
+          'assets/lottie/splash-screen.json',
+          width: 224,
+          height: 224,
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+}
